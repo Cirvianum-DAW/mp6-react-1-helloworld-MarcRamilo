a72527ba5e97aaf11425f17a4d9bd1be60ce55df
MarcRamilo <marc.ramilo@crivanum.cat>
2024-04-17 16:18:58 +0200

    finalitzat

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,44 @@
-import React from "react";
-import { useState } from "react";
-import Form from "./components/form"; // Fix the file path to use the correct casing
-function App() {
-  const [tipusEstudiant, setTipusEstudiant] = useState("no-graduat"); // Afegim la variable d'estat tipusEstudiant
-  const handleChange = (e) => {
+// App.js
+import React, { useState } from "react";
+import Form from "./components/Form";
+
+function App() { // Modul de l'APP
+  const [tipusEstudiant, setTipusEstudiant] = useState("no-graduat"); // Estat de l'APP
+  const [ngPlaces, setNGPlaces] = useState(60); // Número de places disponibles per a no graduats
+  const [gPlaces, setGPlaces] = useState(40); // Número de places disponibles per a graduats
+
+  const handleChange = (e) => { // Funció per a canviar el tipus d'estudiant
     setTipusEstudiant(e.target.value);
-  }; // Afegim la funció que actualitza el valor de la variable d'estat tipusEstudiant
+  };
+
+  const setPlacesDisponibles = (updatedPlaces) => { // Funció per a actualitzar les places disponibles
+    tipusEstudiant === "no-graduat"
+      ? setNGPlaces(updatedPlaces)
+      : setGPlaces(updatedPlaces);
+    console.log("Places Disponibles No Graduats: ", ngPlaces);
+    console.log("Places Disponibles Graduats: ", gPlaces);
+  };
+
   return (
     <div className="flex flex-col items-center justify-center gap-5 h-screen">
-      <div className="tipusEstudiant">
-        <label className="m-4">Selecciona Tipus d'Estudiant:</label>
-
-        <select
-          className="appDropDown border rounded-md py-1 px-2"
-          onChange={handleChange}
-          value={tipusEstudiant}
-        >
-          <option value="no-graduat">No Graduat</option>
-          <option value="graduat">Graduat</option>
-        </select>
-      </div>
-      <Form tipusEstudiantSelect={tipusEstudiant} />
+      <label className="m-4">Selecciona Tipus d'Estudiant:</label>
+      <select
+        className="appDropDown border rounded-md py-1 px-2"
+        onChange={handleChange} // Quan canvia el tipus d'estudiant crida a la funció handleChange
+        value={tipusEstudiant} // Valor actual del tipus d'estudiant
+      >
+        <option value="no-graduat">No Graduat</option>
+        <option value="graduat">Graduat</option>
+      </select>
+      <Form
+        tipusEstudiantSelect={tipusEstudiant} // Propietat tipusEstudiantSelect amb el valor del tipus d'estudiant
+        setPlacesDisponibles={setPlacesDisponibles} // Propietat setPlacesDisponibles amb la funció setPlacesDisponibles
+        ngPlaces={ngPlaces}  // Propietat ngPlaces amb el valor de places disponibles per a no graduats
+        gPlaces={gPlaces}  // Propietat gPlaces amb el valor de places disponibles per a graduats
+        placesActuals={tipusEstudiant === "no-graduat" ? ngPlaces : gPlaces} // Propietat placesActuals amb el valor de places disponibles
+      />
     </div>
   );
 }
+
 export default App;
